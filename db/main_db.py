768aa9e3050e8a542ab0962d6848a9962f4c44ed
Amina <aminakirkeeva777@gmail.com>
2026-06-20 05:49:45 +0000

    hw7

--- db/main_db.py
+++ db/main_db.py
@@ -2,33 +2,31 @@ import sqlite3
 from db import queries
 from config import path_db
 
-
 def init_db():
-    conn = sqlite3.connect(path_db)       # Соединение к БД
-    cursor = conn.cursor()                # Исполнитель который относит запросы к БД
-    cursor.execute(queries.task_table)    # Принимает сам запрос в виде строки
+    conn = sqlite3.connect(path_db)
+    cursor = conn.cursor()
+    cursor.execute(queries.task_table)
     conn.commit()
     conn.close()
-
 
 def add_task(task):
     conn = sqlite3.connect(path_db)
-    cursor = conn.cursor()   # () - это методы которые "живут" внутри обьекта
+    cursor = conn.cursor()
     cursor.execute(queries.insert_task, (task, ))
     conn.commit()
-    task_id = cursor.lastrowid  # это атрибут или параметр который храниться внтури обьекта
+    task_id = cursor.lastrowid
     conn.close()
     return task_id
 
-
-def update_task(task_id, new_task):
+def update_task(task_id, new_task=None, completed=None):
     conn = sqlite3.connect(path_db)
-    cursor = conn.cursor()  
-    # тут строго последовательно
-    cursor.execute(queries.update_task, (new_task, task_id))
+    cursor = conn.cursor()
+    if new_task is not None:
+        cursor.execute(queries.update_tasks, (new_task, task_id))
+    elif completed is not None:
+        cursor.execute(queries.update_completed, (completed, task_id))
     conn.commit()
     conn.close()
-
 
 def get_tasks(filter_type):
     conn = sqlite3.connect(path_db)
@@ -39,9 +37,24 @@ def get_tasks(filter_type):
         cursor.execute(queries.select_tasks_completed)
     elif filter_type == 'uncompleted':
         cursor.execute(queries.select_tasks_uncompleted)
-    
-    tasks = cursor.fetchall()   # all - все, many - несколько
+    tasks = cursor.fetchall()
     conn.close()
     return tasks
+
+def delete_task(task_id):
+    conn = sqlite3.connect(path_db)
+    cursor = conn.cursor()
+    cursor.execute(queries.delete_tasks, (task_id, ))
+    conn.commit()
+    conn.close()
+
+def delete_completed_tasks():
+    conn = sqlite3.connect(path_db)
+    cursor = conn.cursor()
+    cursor.execute(queries.delete_completed)
+    conn.commit()
+    conn.close()
+
+
     
     
--- main.py
+++ main.py
@@ -1,52 +1,74 @@
 import flet as ft
 from db import main_db
 
-
 def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.LIGHT
-
     filter_type = 'all'
-
     task_list = ft.Column()
 
     def load_tasks():
-        task_list.controls.clear()      # всегда очищаем перед добавлением
-        for task_id, task_text, completed in main_db.get_tasks(filter_type):      # принимаем поля из БД
-            task_list.controls.append(view_tasks(task_id=task_id, task_text=task_text))
+        task_list.controls.clear()
+        for task_id, task_text, completed in main_db.get_tasks(filter_type):
+            task_list.controls.append(view_tasks(
+                task_id=task_id,
+                task_text=task_text,
+                completed=completed
+            ))
+        task_list.update()
 
+    def view_tasks(task_id, task_text, completed=None):
+        def toggle_task(task_id, is_completed):
+            main_db.update_task(task_id=task_id, completed=int(is_completed))
+            load_tasks()
 
-    def view_tasks(task_id, task_text):
+        checkbox = ft.Checkbox(
+            value=bool(completed),
+            on_change=lambda e: toggle_task(task_id=task_id, is_completed=e.control.value)
+        )
 
         def save_task(e):
             main_db.update_task(task_id=task_id, new_task=task_field.value)
             task_field.read_only = True
+            task_field.update()
 
         def enable_edit(e):
-            if task_field.read_only == True:
-                task_field.read_only = False
-            else:
-                task_field.read_only = True
+            task_field.read_only = not task_field.read_only
+            task_field.update()
+
+        def delete_task(e):
+            main_db.delete_task(task_id=task_id)
+            task_list.controls.remove(row)
+            task_list.update()
 
         task_field = ft.TextField(read_only=True, value=task_text, expand=True)
         edit_button = ft.IconButton(icon=ft.Icons.EDIT, on_click=enable_edit)
         save_button = ft.IconButton(icon=ft.Icons.SAVE, on_click=save_task)
-        
-        return ft.Row([task_field, edit_button, save_button])
+        delete_button = ft.IconButton(icon=ft.Icons.DELETE, icon_color=ft.Colors.RED_400, on_click=delete_task)
 
+        row = ft.Row([checkbox, task_field, edit_button, save_button, delete_button])
+        return row
 
     def add_task_db(e):
         if task_input.value:
             task = task_input.value
-            # Запись в БД
             task_id = main_db.add_task(task=task)
-            print(f"Задача - {task} добавлена! Его ID - {task_id}")
             task_list.controls.append(view_tasks(task_id=task_id, task_text=task))
+            task_list.update()
             task_input.value = None
+            task_input.update()
 
+    def clear_completed(e):
+        main_db.delete_completed_tasks()
+        load_tasks()
+
+    clear_button = ft.ElevatedButton(
+    'Очистить выполненные',
+    icon_color=ft.Colors.RED_400,
+    on_click=clear_completed
+)
 
     task_input = ft.TextField(label="Введите задачу", expand=True, on_submit=add_task_db)
     task_button = ft.IconButton(icon=ft.Icons.ADD, on_click=add_task_db)
-
     send_task = ft.Row([task_input, task_button])
 
     def set_filter(filter_value):
@@ -54,24 +76,24 @@ def main(page: ft.Page):
         filter_type = filter_value
         load_tasks()
 
-
     filter_buttons = ft.Row([
         ft.ElevatedButton(
-            'All tasks', on_click=lambda e: set_filter('all'), icon=ft.Icons.ALL_INBOX, icon_color=ft.Colors.BLACK_87
-            ),
+            'All tasks', on_click=lambda e: set_filter('all'),
+            icon=ft.Icons.ALL_INBOX, icon_color=ft.Colors.BLACK_87
+        ),
         ft.ElevatedButton(
-            'In a process', on_click=lambda e: set_filter('uncompleted'), icon=ft.Icons.WATCH, icon_color=ft.Colors.YELLOW_900, bgcolor=ft.Colors.YELLOW_100
-            ),
+            'In a process', on_click=lambda e: set_filter('uncompleted'),
+            icon=ft.Icons.WATCH, icon_color=ft.Colors.YELLOW_900, bgcolor=ft.Colors.YELLOW_100
+        ),
         ft.ElevatedButton(
-            'Done', on_click=lambda e: set_filter('completed'), icon=ft.Icons.CHECK_BOX, icon_color=ft.Colors.GREEN_900, bgcolor=ft.Colors.GREEN_100
-            ),
+            'Done', on_click=lambda e: set_filter('completed'),
+            icon=ft.Icons.CHECK_BOX, icon_color=ft.Colors.GREEN_900, bgcolor=ft.Colors.GREEN_100
+        ),
     ], alignment=ft.MainAxisAlignment.SPACE_AROUND)
 
-
-    page.add(send_task, filter_buttons, task_list)
+    page.add(send_task, filter_buttons, task_list, clear_button)
     load_tasks()
-
 
 if __name__ == "__main__":
     main_db.init_db()
-    ft.run(main, view=ft.AppView.WEB_BROWSER)
+    ft.app(main, view=ft.AppView.WEB_BROWSER)
